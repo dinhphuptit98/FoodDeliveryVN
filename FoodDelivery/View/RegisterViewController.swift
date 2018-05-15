5798caf09b73ba3f0236ad7d3c74669055196354
NguyenDinhPhu <nguyendinhphu2909@gmail.com>
2018-05-15 18:11:58 +0700

    update3

--- FoodDelivery/View/RegisterViewController.swift
+++ FoodDelivery/View/RegisterViewController.swift
@@ -8,45 +8,60 @@
 
 import UIKit
 
-class REGISTERViewController: UIViewController , UITextFieldDelegate{
-
+class REGISTERViewController: UIViewController , UITextFieldDelegate {
+    
     @IBOutlet weak var hovatenText: UITextField!
     @IBOutlet weak var numberPhoneText: UITextField!
-    @IBOutlet weak var dangkyButton: UIButton!
+    @IBOutlet weak var dangky: UIButton!
+    
+    @IBOutlet weak var whiteView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         hovatenText.delegate = self
         numberPhoneText.delegate = self
-        dangkyButton.isEnabled = false
+        
+        
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        if UserDefaults.standard.string(forKey: "sdt") != "" && UserDefaults.standard.string(forKey: "hovaten") != "" {
+            whiteView.isHidden = false
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
     @IBAction func dangkyBt(_ sender: UIButton) {
-        sender.isEnabled = false
-        if hovatenText.text != nil && numberPhoneText.text != nil {
-            dangkyButton.isEnabled = true
-            sender.isEnabled = true
-        }
+        
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        hovatenText.resignFirstResponder()
-        numberPhoneText.resignFirstResponder()
+        textField.resignFirstResponder()
         return true
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if hovatenText.text != "" && numberPhoneText.text != "" {
+            dangky.isEnabled = true
+            UserDefaults.standard.set(hovatenText.text, forKey: "hovaten")
+            UserDefaults.standard.set(numberPhoneText.text, forKey: "sdt")
+            print(String(UserDefaults.standard.string(forKey: "sdt")!))
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    }
 }
